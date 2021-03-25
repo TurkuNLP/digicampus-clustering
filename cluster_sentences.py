1f0abbe3ad0c525d5b73132c1b75ce3d7822926f
Iiro Rastas <11477684+Itara20@users.noreply.github.com>
2021-03-25 16:54:35 +0200

    Add clustering and keywords

---
+++ cluster_sentences.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import argparse, json, math
+import numpy as np
+from scipy.sparse import issparse
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import cluster
+from pathlib import Path
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="The script takes in a file of sentences, and another file with the sentence embeddings, and outputs the clustered sentences.")
+    parser.add_argument("--sentence-file", type=str, required=True, help="Path to the file containing all the sentences.")
+    parser.add_argument("--embedding-file", type=str, required=True, help="Path to the file containing all the sentence embeddings.")
+    parser.add_argument("--outdir",type=str, required=True, help="Directory where the clustered output will be saved to as a JSON file.")
+    parser.add_argument("--num-labels", type=int, help="The number of labels to use for clustering. Default: Three times the square root of the number of sentences, rounded down.")
+    return parser 
+
+def cluster_sentences(sentence_lists, num_labels=None):
+    sentences = [s for l in sentence_lists for s in l]
+    vectorizer = TfidfVectorizer(ngram_range=(2, 5), analyzer='char_wb').fit(sentences)
+    vectors = vectorizer.transform(sentences)
+    clustering_model = cluster.AgglomerativeClustering(n_clusters=num_labels if num_labels else 3*int(math.sqrt(len(sentences))))
+    # clustering_model = cluster.AffinityPropagation(random_state=0, damping=0.7)
+    # clustering_model = cluster.OPTICS(min_samples=2)
+    # clustering_model = cluster.KMeans(n_clusters=args.num_labels, random_state=0)
+
+    clusters = clustering_model.fit(vectors.toarray()).labels_
+    clustered_lists = []
+    i = 0
+    for l in sentence_lists:
+        clustered_list = []
+        for s in l:
+            clustered_list.append((s, clusters[i]))
+            i += 1
+        clustered_lists.append(clustered_list)
+
+    return clustered_lists
+
+def get_keywords(clustered_lists, num_keywords=3):
+    #sentences_tokenize = [nltk.word_tokenize(sentence.lower()) for sentence in sentences]
+    ##sentences_stem = sentences_tokenize
+    ##sentences_stem = [[stemmer.stem(token) for token in sentence] for sentence in sentences_tokenize]
+    #sentences_vectorize = vectorizer.fit_transform([' '.join(sentence) for sentence in sentences_stem])
+    vectorizer = TfidfVectorizer().fit(s for l in clustered_lists for s, _ in l)
+    cluster_dict = {}
+    for l in clustered_lists:
+        for s, c in l:
+            cluster_dict.setdefault(c, []).append(s)
+
+    cluster_keywords = {}
+    for cluster_id, sl in cluster_dict.items():
+        feat_freq = vectorizer.transform(sl).toarray().sum(axis=0).squeeze()
+        max_idx = np.argsort(feat_freq)[-num_keywords:][::-1]
+        cluster_keywords[cluster_id] = [vectorizer.get_feature_names()[i] for i in max_idx]
+
+    return cluster_keywords
+
+# if __name__=="__main__":
+#     args = get_parser().parse_args()
+# 
+#     with open(args.sentence_file, 'rt') as f:
+#         sentences = [line.rstrip('\r\n') for line in f]
+# 
+#     print(f"{len(sentences)} of sentences read")
+#     num_labels = args.num_labels if args.num_labels else 3*int(math.sqrt(len(sentences)))
+#     print(f"Clustering to {num_labels} groups")
+#     vectors = np.load(args.embedding_file, allow_pickle=True)
+#     
+#     if issparse(vectors):
+#         vectors = vectors.toarray()
+# 
+#     clustering = cluster_sentences(vectors, num_labels).tolist()
+# 
+#     dict_list = [{'sentence_id': i, 'sentence': s, 'cluster_id': c} for i, s, c in zip(range(len(sentences)), sentences, clustering)]
+#     
+#     filename = Path(args.outdir) / ('sentences_clustered' + '.json')
+#     with open(filename, 'w') as f:
+#         json.dump(dict_list, f, sort_keys=True, indent=4, ensure_ascii=False)
+#     print(f"Clustered sentences saved to {filename}")
--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,7 @@ import sys
 
 from read import read_files
 from preprocessing import preproc_udpipe
+from cluster_sentences import cluster_sentences, get_keywords
 
 
 if __name__ == "__main__":
@@ -12,4 +13,5 @@ if __name__ == "__main__":
     data = read_files(filenames) # read json files
     data = [d["essay"] for d in data] # get the essay text from the dictionary item
     data = preproc_udpipe(data) # sentence segmentation by udpipe
-
+    data = cluster_sentences(data)
+    keyword_dict = get_keywords(data)
