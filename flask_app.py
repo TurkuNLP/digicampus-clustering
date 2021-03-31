671fe314b57cce5f08e9073bc92be21452c98f4a
Filip Ginter <filip.ginter@gmail.com>
2021-03-31 14:40:40 +0300

    colors and mouseovers

--- flask_app.py
+++ flask_app.py
@@ -9,6 +9,7 @@ import datetime
 import html
 import re
 from doc import Doc, DocCollection, load_doc_collection
+import numpy as np
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -45,6 +46,27 @@ def answer(exam_id,answer_idx,method):
         clusters=exam.TFIDF_clusters
         keywords=exam.TFIDF_keywords
 
+    for k,v in keywords.items():
+        keywords[k]=", ".join(v)
+
+
     #Let's perhaps prepare the data for the template here, while we are still in Python
-    return render_template("answer.html",exam=exam,answer=answer,sentences_and_clusters=clusters[answer_idx],examapp_root=APP_ROOT)
+    num_clust=len(keywords)
+    clust_sizes=[0 for _ in range(num_clust)]
+    for clustered_answer in clusters:
+        for sent_text,clust_id in clustered_answer:
+            clust_sizes[clust_id]+=1
+    clust_sizes=np.array(clust_sizes)
+
+    clust_sizes=(clust_sizes/clust_sizes.sum())
+    clust_values=(clust_sizes/clust_sizes.max()*70).astype(np.int) #this is lightness value
+    clust_hues=np.arange(0,300,(300-0)/num_clust).astype(np.int)
+
+    ### HERE I PREPARE EVERYTHING I NEED TO VISUALIZE THE TEXT
+    ### SO THIS CAN THEN BE LOOPED OVER IN JINJA
+    sentences_and_clusters=[]
+    for s_text,cls in clusters[answer_idx]:
+        sentences_and_clusters.append((s_text,cls,clust_hues[cls],clust_values[cls]))
+
+    return render_template("answer.html",exam=exam,answer=answer,sentences_and_clusters=sentences_and_clusters,keywords=keywords,examapp_root=APP_ROOT)
 
