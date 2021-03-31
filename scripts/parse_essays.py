54675f295b73b2706e14a40d062829c7abcc2973
Jenna Kanerva <jmnybl@utu.fi>
2021-03-31 15:30:23 +0300

    preserve spaceafter

--- scripts/parse_essays.py
+++ scripts/parse_essays.py
@@ -12,8 +12,15 @@ extra_args=types.SimpleNamespace()
 extra_args.__dict__["udify_mod.device"]="0" #simulates someone giving a --device 0 parameter to Udify
 extra_args.__dict__["lemmatizer_mod.device"]="0"
 
-available_pipelines=read_pipelines("models_fi_tdt_v2.7/pipelines.yaml")               # {pipeline_name -> its steps}
-p=Pipeline(available_pipelines["parse_plaintext"], extra_args)                                      # launch the pipeline from the steps
+available_pipelines=read_pipelines("models_fi_tdt_v2.7/pipelines.yaml")        # {pipeline_name -> its steps}
+turku_segmenter=Pipeline(available_pipelines["tokenize"])         # launch the pipeline from the steps
+
+conllu_pipeline = available_pipelines["parse_conllu"]
+if conllu_pipeline[0].startswith("extraoptions"):
+    extraoptions=conllu_pipeline[0].split()[1:] # ['--empty-line-batching']
+    conllu_pipeline.pop(0)
+    extra_args.__dict__["empty_line_batching"]=True
+turku_parser=Pipeline(conllu_pipeline, extra_args)
 
 
 
@@ -35,12 +42,29 @@ def read_conllu(txt):
         if sent:
             yield comment, sent
 
+def merge_conllu(segmented, parsed):
+    # udify deletes spaceafter, merge udpipe and udify to get full conllu
+    conllu = []
+    for (s_comm, s_sent), (p_comm, p_sent) in zip(segmented, parsed):
+    
+        assert s_comm == p_comm
+        for i, token in enumerate(s_sent):
+            p_sent[i][MISC] = token[MISC] # transfer MISC field, all the rest are fine.
+        conllu.append((p_comm, p_sent))
+        
+    return conllu
+
 
 def parse(txt):
 
-    txt_parsed = p.parse(txt)
-    conllu = [(comm, sent) for comm, sent in read_conllu(txt_parsed)]
+    tokenized = turku_segmenter.parse(txt)
+    parsed = turku_parser.parse(tokenized)
+    conllu_seg = [(comm, sent) for comm, sent in read_conllu(tokenized)]
+    conllu_par = [(comm, sent) for comm, sent in read_conllu(parsed)]
+    conllu = merge_conllu(conllu_seg, conllu_par)
     return conllu
+
+
 
 def parse_all(data):
     examples = []
